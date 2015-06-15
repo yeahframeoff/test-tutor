0f16791e42acc07652e6e27ac793309abfd0bb38
yeahframeoff <anthyef@gmail.com>
2015-06-15 20:39:16 +0300

    FT now adds one more item, home page now processes POST request

--- functional_tests.py
+++ functional_tests.py
@@ -36,20 +36,25 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # there is still text-box inviting to enter more items.
         # user enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # the page updates again, and now both items are shown in the list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # user wonders, whether the site will remember his list.
         # then he notices that the site has generated a unique url for him 
         # -- there is some explanatory text to that effect
+        self.fail('Finish test!');
 
         # user visits that url - his to-do list is still there
 
