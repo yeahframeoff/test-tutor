c6a336f5bc4893e764b1f8f160f1085488728ff4
yeahframeoff <anthyef@gmail.com>
2015-06-15 16:55:16 +0300

    functional test now checks if we can input a todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,16 +16,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # user sees that site's title mentions to-do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
 
         # invitation to enter a to-do item straight away
-        # user types "Buy peacock feathers " into a text box
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # user types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # when user hits enter, the page updates, and now the page lists 
         # "1. Buy peacock feathers" as an item of a to-do list
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # there is still text-box inviting to enter more items.
         # user enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
 
         # the page updates again, and now both items are shown in the list
 
