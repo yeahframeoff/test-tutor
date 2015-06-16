e51dbf4b7e95ce79bf2954f8111a04c9fc02ce84
yeahframeoff <anthyef@gmail.com>
2015-06-16 13:25:26 +0300

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,5 +5,7 @@ from lists import views
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/',
+        views.view_list, name='view_list')
 ]
